ef9886e587cef3813e5a5211ea95102a201bc7d5
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-05-17 02:06:21 +0300

    todays changes

---
+++ first-react-app/src/App.tsx
@@ -0,0 +1,32 @@
+//import Message from "./message"
+//import ListGroup from "./Components/ListGroup";
+
+function App() {
+  let items = ["New York", "Paris", "Nairobi", "Nakuru"];
+  items = [];
+
+  //code duplication in programming is not advisable
+  /* if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No items found!</p>
+      </>
+    );
+  */
+//const message = items.length === 0 ? <p>No item found!</p> : null;
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>NO item found!</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ first-react-app/src/message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message () {
+    //this is JSX : JavaScript XML
+    const name = 'Sheddiey';
+    if (name) 
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message;
