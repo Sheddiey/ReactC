07af407039b1007e9a5989519511208ef8fce3a6
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-05-28 18:54:52 +0300

    Arrow functions

---
+++ ES6-for-react/4.js
@@ -0,0 +1,17 @@
+console.clear()
+
+function greetings(name) {
+    return `welcome ${name}`;
+}
+
+
+
+
+const greeting = (name1, age) => {
+    return `Welcome ${name1} ${age}`;
+}
+
+const mGreeting = name1 => `Welcome ${name1}`
+
+console.log(greeting('Sheddiey', 21));
+console.log('Shaddy');
