f563b44d0f68bc650cec7f4788aa3c46f42966fe
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-05-28 04:58:44 +0300

    creating variables

---
+++ ES6-for-react/2.js
@@ -0,0 +1,11 @@
+//object
+let person ={
+    name : 'Shadrack',
+    age : '21',
+    skinColor : 'black'
+}
+
+//assigning a value to a variable before declaring it
+num = 5;
+console.log(num);
+var num;
