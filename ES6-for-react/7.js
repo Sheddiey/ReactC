10f0e4759963fe7ff5097a628464672012316992
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-05-29 05:13:04 +0300

    Destructuring

---
+++ ES6-for-react/7.js
@@ -0,0 +1,37 @@
+console.clear();
+
+//const name = "Shadrack Cheruiyot Sheddiey"
+//const mArr1 = name.split(' ');
+
+//console.log(mArr1);
+//let fName = mArr1[0];
+//let sName = mArr1[1];
+
+//to skip some elements you can select blank spaces
+//let [fName, ,lName] = mArr1;
+
+//console.log(fName);
+//console.log(sName);
+//console.log(lName);
+
+/*const person1 = {
+    fName: 'Shadrack',
+    mName: 'Cheruiyot',
+    lName: 'Sheddiey',
+    age: 21
+}
+
+let {fName, lName} = person1;
+console.log(fName);
+console.log(lName); */
+let fName = 'SHeddiey';
+let lName = 'kevin';
+let age = 21;
+
+const personObj = {
+    fName,
+    lName,
+    age
+}
+
+console.log(personObj);
