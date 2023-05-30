da6793f36ccb5b3f1ccf59d8c971075b237cb0b4
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-05-30 18:54:39 +0300

    find and finnIndex updates

--- ES6-for-react/11.js
+++ ES6-for-react/11.js
@@ -1,7 +1,23 @@
 console.clear();
 
-const numArr = [-1, -2, -3, -4, 1, 2, 3, 4];
+/* const numArr = [-1, -2, -3, -4, 1, 2, 3, 4];
 
 numArr.find(item => {
     console.log(item);
-});
+});  */
+
+const numArr1 = [-1, -2, -3, -4, 1, 2, 3, 4, 20]
+
+//const result = numArr.find(item => {
+    ///return true;
+    //to return the first even number  && to get a number greater than 0
+   // return item % 2 === 0 && item >= 0;  
+//})
+
+const result = numArr1.findIndex(item => { //to find the positioon of the first positive even number
+    return item % 5 === 0 && item >= 0;  
+})
+
+
+console.log('Result =>', result);
+
