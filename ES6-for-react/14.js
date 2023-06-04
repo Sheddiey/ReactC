a927fb6dac2c57b6c98f067a82a616f974bf3e57
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-04 08:51:56 +0300

    then() and catch() methods

--- ES6-for-react/14.js
+++ ES6-for-react/14.js
@@ -1,10 +1,20 @@
 console.clear();
 
 const postListPromise = new Promise ((resolve, reject) => {
-    $.get('https://github.com/Sheddiey', (data) => {
-        console.log('GET POST LIST Response => ', data);
-        resolve(data);
+    $.get('https://jsonplaceholder.typicode.com/posts', (data) => {
+        //console.log('GET POST LIST Response => ', data);
+        resolve(100);
     }).fail(err => {
-        reject(new Error(`Call failed for POst List Reequest with status ${err.status}`))
-    })
+        reject(new Error(`Call failed for POst List Request with status ${err.status}`))
+    });
+});
+
+postListPromise
+.then((response) => {
+    console.log("call success");
+    console.log('Then response =>', response);
 })
+.catch((error) => {
+    console.log("Call failed");
+    console.log('Catch error =>', error);
+});
