f839a1d805b709b279d9244438db60b21efeb62e
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-06 12:33:50 +0300

    how react works

--- React/react-app/src/main.tsx
+++ React/react-app/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  //react dom is used to render the component below
+  //...inside and element with an id of root
   <React.StrictMode>
     <App />
   </React.StrictMode>,
