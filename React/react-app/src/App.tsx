606c2cf9ec4141b8324299e6b91dca33472ae84c
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-06 13:41:34 +0300

    Fragments

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -1,19 +1,23 @@
 //import Message  from "./message";
 import ListGroup from "./Components/ListGroup";
+//import { Fragment } from "react"; use empty angle brackets
 
 function App() {
   //JSx : JavaScript XML
   //return <div><Message></Message></div>;
   //return <div><ListGroup></ListGroup></div>
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
-  );//parenthesis to spread jsx into multiple lines
+    //a better way is to use fragments instead of a div element
+    <> 
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </> );//parenthesis to spread jsx into multiple lines
 }
 
 export default App;
