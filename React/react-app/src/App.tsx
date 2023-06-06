54c959938a23e1f597febe9c413da8e3110ec01b
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-06 14:12:43 +0300

    conditional rendering

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -6,12 +6,28 @@ function App() {
   //JSx : JavaScript XML
   //return <div><Message></Message></div>;
   //return <div><ListGroup></ListGroup></div>
-  const items = ["New York", "Kampala", "Nairobi", "Tunis", "Algiers"];
+  let items = ["New York", "Kampala", "Nairobi", "Tunis", "Algiers"];
+  items = [];
 
+  // if (items.length === 0) //repetion is not instead we are gonna render conditionally
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>;
+  //     </>
+  //   );
+
+  //const message = items.length === 0 ? <p>No item found</p> : null;
+  //the benefits of using functions is that they can have parameters
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // };
   return (
     //a better way is to use fragments instead of a div element
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null}; */}
+      {items.length === 0 && <p>No item found</p>};
       {items.map((item) => (
         <li key={item}>{item}</li>
       ))}
