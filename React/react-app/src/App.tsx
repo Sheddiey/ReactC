7e0b1f3315de5007d65d54e9f74440505ab93af6
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-06 13:00:29 +0300

    creating list group components

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -1,9 +1,19 @@
-import Message  from "./message";
+//import Message  from "./message";
+import ListGroup from "./Components/ListGroup";
 
-function App () {
-   //JSx : JavaScript XML
-  return <div><Message></Message></div>;
+function App() {
+  //JSx : JavaScript XML
+  //return <div><Message></Message></div>;
+  //return <div><ListGroup></ListGroup></div>
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );//parenthesis to spread jsx into multiple lines
 }
-
 
 export default App;
