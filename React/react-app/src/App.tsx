36f4d023ab9c4b00627b74defcb5501d6df2913b
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-07 21:07:05 +0300

    handling event

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   //JSx : JavaScript XML
   //return <div><Message></Message></div>;
   //return <div><ListGroup></ListGroup></div>
-  let items = ["New York", "Kampala", "Nairobi", "Tunis", "Algiers"];
-  items = [];
+  return ListGroup();
+
 
   // if (items.length === 0) //repetion is not instead we are gonna render conditionally
   //   return (
@@ -22,17 +22,6 @@ function App() {
   // const getMessage = () => {
   //   return items.length === 0 ? <p>No item found</p> : null;
   // };
-  return (
-    //a better way is to use fragments instead of a div element
-    <>
-      <h1>List</h1>
-      {/* {items.length === 0 ? <p>No item found</p> : null}; */}
-      {items.length === 0 && <p>No item found</p>};
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </>
-  ); //parenthesis to spread jsx into multiple lines
 }
 
 export default App;
--- React/react-app/src/Components/ListGroup.tsx
+++ React/react-app/src/Components/ListGroup.tsx
@@ -1,6 +1,31 @@
+import { MouseEvent } from "react";
 
-function ListGroup () {
-    return <h1>List Group</h1>;
+function ListGroup() {
+  let items = ["New York", "Kampala", "Nairobi", "Tunis", "Algiers"];
+
+
+  //event handler 
+  const handleClick = (event: MouseEvent) => {console.log(event)}
+
+  return (
+    //a better way is to use fragments instead of a div element
+    <>
+      <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null}; */}
+      {items.length === 0 && <p>No item found</p>};
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  ); //parenthesis to spread jsx into multiple lines
 }
 
 export default ListGroup;
