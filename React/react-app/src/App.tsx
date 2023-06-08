56afdc95278ff8d4bca4d0fa2102e47d296cdc67
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-06-08 21:36:49 +0300

    passing children part-1

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -3,40 +3,29 @@ import ListGroup from "./Components/ListGroup";
 //import { Fragment } from "react"; use empty angle brackets
 
 function App() {
-  let items = ["New York", "Kampala", "Nairobi", "Tunis", "Algiers"];
-
-const handleSelectItem = (item: string) => {
-  console.log(item);
-}
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+      <Alert />
     </div>
   );
 }
 
 export default App;
 
+//JSx : JavaScript XML
+//return <div><Message></Message></div>;
+//return <div><ListGroup></ListGroup></div>
 
+// if (items.length === 0) //repetion is not instead we are gonna render conditionally
+//   return (
+//     <>
+//       <h1>List</h1>
+//       <p>No item found</p>;
+//     </>
+//   );
 
-
- //JSx : JavaScript XML
-  //return <div><Message></Message></div>;
-  //return <div><ListGroup></ListGroup></div>
-  
-
-
-  // if (items.length === 0) //repetion is not instead we are gonna render conditionally
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No item found</p>;
-  //     </>
-  //   );
-
-  //const message = items.length === 0 ? <p>No item found</p> : null;
-  //the benefits of using functions is that they can have parameters
-  // const getMessage = () => {
-  //   return items.length === 0 ? <p>No item found</p> : null;
-  // };
+//const message = items.length === 0 ? <p>No item found</p> : null;
+//the benefits of using functions is that they can have parameters
+// const getMessage = () => {
+//   return items.length === 0 ? <p>No item found</p> : null;
+// };
